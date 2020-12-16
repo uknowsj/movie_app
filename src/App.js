3e9baef6a1aec008594ca2b9ba8c5b08085aae7f
uknowsj <44722864+uknowsj@users.noreply.github.com>
2020-12-16 17:22:59 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 import React from "react"
+import PropTypes from "prop-types"
+import axios from "axios"
+import Movies from "./Movies"
+import "./App.css"
 
-const foodInfo =[
-  {
-    id : 1,
-    name : "kimbap",
-    img : "http://loremflickr.com/300/300"
-  },
-  {
-    id : 2,
-    name : "soondae",
-    img : "http://loremflickr.com/300/300"
-  },
-  {
-    id :3,
-    name : "tteokbokki",
-    img : "http://loremflickr.com/300/300"
+class App extends React.Component{ //리액트 컴포넌트의 스테이트를 쓰기위해 클래스 컴포넌트 사용
+  state={
+    isLoading : true,
+    moviesArray : [],
+  };
+  //axios가 느리기 때문에 axios로딩될 때 까지 기다리라고 하고싶음
+  getMovies=async()=>{
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    //movies.data.data.movies
+    this.setState({moviesArray:movies,isLoading:false});
   }
-];
-
-function Food({name,img}){
-  return (
-  <div>
-    <h3>I love {name}</h3>
-    <img src={img}></img>
-  </div>
-  )
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const {isLoading,moviesArray} = this.state; 
+    return (
+    <section className="container">
+      {isLoading?
+        (
+          <div className="loader">
+            <span className="loader__text">"Loading.."</span>
+          </div>
+        ):
+        (
+          <div className="movies">
+            {moviesArray.map(movie=>{
+              return (
+              <Movies 
+                key={movie.id} 
+                id={movie.id}  
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+                genres = {movie.genres} 
+                year = {movie.year} 
+                />)
+            })}
+          </div>
+        )
+      }
+      
+    </section>
+)
+  };
 }
-
-function App() {
-  return (
-  <div className="App" >
-    {foodInfo.map(dish =>
-      <Food key={dish.id} name={dish.name} img={dish.img} />
-    )}
-  </div>
-  );
-}
-
 export default App;
